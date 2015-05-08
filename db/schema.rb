62e3e3f109cf5a54684a8ed1757e20134c066599
Sohailvaghari@gmail.com <sohailvaghari@gmail.com>
2015-05-08 01:27:39 +0000

    Added custom fields to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150507003705) do
+ActiveRecord::Schema.define(version: 20150508012006) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -40,6 +40,9 @@ ActiveRecord::Schema.define(version: 20150507003705) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "phone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
